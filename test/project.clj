6d5b30f5a16bbd4dfc6e90d2dabb676f0e6924cc
Martin Trojer <martin.trojer@gmail.com>
2012-03-03 17:40:31 +0000

    remove files in test folder

--- test/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject frinj "0.1.0"
-  :description "Frinj the unit-of-measure calculator Clojure DSL"
-  :url "https://github.com/martintrojer/frinj"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
