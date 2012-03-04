30a3f5d4350d9147ee302d6172705f0e031608f6
Martin Trojer <martin.trojer@gmail.com>
2012-03-04 09:45:45 +0000

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject frinj "0.1.1"
+(defproject frinj "0.1.2"
   :description "Practical unit-of-measure calculator DSL for Clojure"
   :url "https://github.com/martintrojer/frinj"
   :dependencies [[org.clojure/clojure "1.3.0"]])
