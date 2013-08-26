060f322d473c49d20ed70c43b1e14e49a7183290
Martin Trojer <martin.trojer@gmail.com>
2013-08-26 11:47:13 +0100

    back on snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject frinj "0.2.1"
+(defproject frinj "0.2.2-SNAPSHOT"
   :description "Practical unit-of-measure calculator DSL for Clojure"
   :url "https://github.com/martintrojer/frinj"
   :dependencies [[org.clojure/clojure "1.5.1"]]
